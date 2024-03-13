7ae531c76738295e72c10bf93552e22d31cb7911
Gustavo Oliveira <Aluno@fatecdiadema.com>
2024-03-13 09:20:24 -0300

    Adicionando configuração de origem

--- zero_fome/settings.py
+++ zero_fome/settings.py
@@ -26,12 +26,12 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-server-production-774a.up.railway.app']
+ALLOWED_HOSTS = ['django-server-production-774a.up.railway.app', 'https://django-server-production-774a.up.railway.app']
 
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-# CSRF_TRUSTED_ORIGINS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['django-server-production-774a.up.railway.app', 'https://django-server-production-774a.up.railway.app']
 
 # Application definition
 
