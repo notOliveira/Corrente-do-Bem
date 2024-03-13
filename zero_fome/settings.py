f5bc48cfe3951ecf688f877b7084e2ff6d128f28
Gustavo Oliveira <Aluno@fatecdiadema.com>
2024-03-13 09:16:48 -0300

    Mudando allowed hosts

--- zero_fome/settings.py
+++ zero_fome/settings.py
@@ -26,7 +26,8 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["*"]
+ALLOWED_HOSTS = ['django-server-production-774a.up.railway.app']
+
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
