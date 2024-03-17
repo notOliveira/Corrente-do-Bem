556ec2585c0e9a8c6d03806fc3f56a17d3d21663
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-03-17 14:53:16 -0300

    Mudando domínios confiáveis

--- zero_fome/settings.py
+++ zero_fome/settings.py
@@ -34,12 +34,12 @@ SECRET_KEY = 'django-insecure-rc^*w^w&6g9_(uvx#6s*bnt!w)l0rdi%!l7mv#y%uc&x%wo5pk
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['django-server-production-774a.up.railway.app', 'https://django-server-production-774a.up.railway.app', 'localhost']
+ALLOWED_HOSTS = ['zero-fome-production.up.railway.app', 'localhost']
 
 
 # FORM SUBMISSION
 # Comment out the following line and place your railway URL, and your production URL in the array.
-CSRF_TRUSTED_ORIGINS = ['https://django-server-production-774a.up.railway.app']
+CSRF_TRUSTED_ORIGINS = ['zero-fome-production.up.railway.app']
 
 # Application definition
 
