a51bcd3c1aebaa95440ae56c3d435f395d445bdd
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-03-30 19:45:31 -0300

    Adicionando urls e estrutura do app donations

---
+++ donations/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views as main_views
+
+urlpatterns = [
+    # path('get-viacep/<str:cep>/', main_views.get_cep, name='get_cep'),
+    # path('', main_views.home, name='home'),
+    # path('near-you', main_views.near_you, name='near-you'),
+    # path('donate', main_views.donate, name='donate'),
+    # path('news', main_views.news, name='news'),
+    # path('news-detail', main_views.news_detail, name='news-detail'),
+]
--- zero_fome/urls.py
+++ zero_fome/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
     path('', include('users.urls')),
-    path('organizations/', include('organizations.urls'))
+    path('', include('donations.urls')),
+    path('organizations/', include('organizations.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
