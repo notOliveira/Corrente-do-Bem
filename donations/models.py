517c0c4fe8f99f3ea13b84b438bf954a6a2c5a98
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-03-30 17:04:55 -0300

    Adicionando módulo de doações

---
+++ donations/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+from users.models import CustomUser as User
+from organizations.models import Organization
+
+class Donation(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    organization = models.ForeignKey(Organization, on_delete=models.CASCADE)
+    date = models.DateTimeField(auto_now_add=True)
+    description = models.TextField(blank=True, null=True)
+    
+    def upload_to_directory(instance, filename):
+        # Defina o caminho de upload com base no ID da organização
+        return f'donations/{instance.organization_id}/{filename}'
+
+    image = models.ImageField(upload_to=upload_to_directory, blank=True, null=True)
+
+    class Meta:
+        verbose_name = 'Doação'
+        verbose_name_plural = 'Doações'
+
