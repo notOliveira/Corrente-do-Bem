3c9847882018690ea73730196f3195d6e594a700
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-07-15 15:19:04 -0300

    Adicionando função para validar senha

---
+++ static/js/password-correct.js
@@ -0,0 +1,26 @@
+const passwordCheck = document.getElementById('password-check');
+const passwordInput1 = document.getElementById('password1');
+const passwordInput2 = document.getElementById('password2');
+
+passwordInput1.addEventListener('input', function() {
+
+    // // p1 =! p2 = senhas diferem
+    // if (passwordInput1 != passwordInput2) {
+    // } 
+    // // p1 ou p2 fora das regras
+    // else if () {
+    // } 
+    // // senhas corretas
+    // else if () {
+    // } 
+    // // regra
+    // else if () {
+    // } 
+    // // regra
+    // else if () {
+    // } 
+    // // regra
+    // else if () {
+    // }
+    
+})
