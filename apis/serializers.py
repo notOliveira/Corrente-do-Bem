04a981ea82c707945ca2238079a4eadbc54aa2c5
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-07-29 14:23:31 -0300

    Melhorando os campos da API, preparando para lista de pesquisa

--- apis/serializers.py
+++ apis/serializers.py
@@ -1,7 +1,14 @@
 from rest_framework import serializers
 from organizations.models import Donation
+from users.models import CustomUser
+
+class CustomUserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CustomUser
+        fields = ['id', 'username']
 
 class DonationSerializer(serializers.ModelSerializer):
+    user = CustomUserSerializer()
     class Meta:
         model = Donation
-        fields = '__all__'
+        fields = ['id', 'user', 'date', 'organization']
