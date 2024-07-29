08acd60d809fcf17c35df8ab87488059f9abcfa6
Gustavo Oliveira <gustavooliveira.classroom@gmail.com>
2024-07-29 14:05:42 -0300

    Adicionando params na API

--- apis/views.py
+++ apis/views.py
@@ -1,4 +1,3 @@
-from django.shortcuts import render
 from rest_framework import viewsets
 from organizations.models import Donation
 from .permissions import CreateSuperUserPermission
@@ -8,3 +7,13 @@ class DonationsViewSet(viewsets.ModelViewSet):
     queryset = Donation.objects.all()
     serializer_class = DonationSerializer
     permission_classes = [CreateSuperUserPermission]
+
+    def get_queryset(self):
+        queryset = Donation.objects.all()
+
+        # Filters
+        organization_id = self.request.query_params.get('organization_id')
+
+        if organization_id is not None:
+            queryset = queryset.filter(organization__id=organization_id)
+        return queryset
